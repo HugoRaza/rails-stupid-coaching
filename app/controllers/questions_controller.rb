9b6d0cf073269166c79f562823568e1929a3f821
hugo razafindranaly <hugo.raza@gmail.com>
2019-02-11 12:45:00 +0100

    exo 1 + optionnal

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,21 @@
+class QuestionsController < ApplicationController
+  def ask
+
+  end
+
+  def answer
+    @answer = params[:question]
+    if @answer['?']
+      @test = "Silly question, get dressed and go to work!"
+    elsif @answer == "I am going to work"
+      @test = "Great!"
+    else
+      @test = "I don't care, get dressed and go to work!"
+    end
+  end
+end
+
+
+# If the message is I am going to work, the coach will answer Great!
+# If the message has a question mark ? at the end, the coach will answer Silly question, get dressed and go to work!.
+# Otherwise he will answer I don't care, get dressed and go to work!
